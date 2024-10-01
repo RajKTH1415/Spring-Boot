736754bba37a96a2a268d67151fae052207dd18e
RajKTH1415 <rajkumarprasadkth@gmail.com>
2024-10-01 19:34:20 +0530

    Update jdbc URL

---
+++ src/main/java/com/te/ems/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.te.ems.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Employee {
+    @Id
+    private String id;
+    private String employeeId;
+    private String firstname;
+    private String lastname;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfJoining;
+    private Double salary;
+
+
+    private Gender gender;
+
+}
