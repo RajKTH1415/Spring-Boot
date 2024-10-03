a797bf0a66c78aa2f39e3361bf0ba010ff6f767c
RajKTH1415 <rajkumarprasadkth@gmail.com>
2024-10-03 11:47:15 +0530

    Required entity classes are created

--- src/main/java/com/te/ems/entity/Employee.java
+++ src/main/java/com/te/ems/entity/Employee.java
@@ -1,22 +1,21 @@
 package com.te.ems.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Builder
 @Entity
 public class Employee {
     @Id
     private String id;
+    @Column(unique = true)
     private String employeeId;
     private String firstname;
     private String lastname;
@@ -26,5 +25,14 @@ public class Employee {
 
     @Enumerated
     private Gender gender;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "employee")
+    private List<Address> addresses;
+
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    private BankAccount bankAccount;
+
+    @ManyToMany(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private List<Technology> technologies;
 
 }
